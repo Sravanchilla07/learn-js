24fc4a1c1b1d73c7a26a171e1416589a435fd6d6
Sravanchilla07 <149558631+Sravanchilla07@users.noreply.github.com>
2023-11-02 06:26:07 +0000

    datatype conversions

---
+++ 01_basics/dt_conversions.js
@@ -0,0 +1,40 @@
+let runs="45"
+//console.log(typeof runs);
+//console.log(typeof(runs));
+let val=Number(runs);
+//console.log(typeof val);
+//"45"=>45
+//"45abc"=>NaN-Not a Number
+//true=>1,false=>0
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+//1=>true;0=>false
+//""=> false
+//"Sravan"=>true
+let num=33;
+let stringNum=String(num);
+//console.log(stringNum);
+//console.log(typeof stringNum);
+//  ******  OPERATIONS  ******
+let value=7;
+// let negValue= -value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+let str1="good"
+let str2=" morning"
+//console.log(str1+str2);
+// console.log("2"+3);
+// console.log(2+"3");
+// console.log("2"+3+3);
+// console.log(2+3+"3"); // ToPrimitive(input[,preferredType])
+// console.log(2+4*5/3);//operator precedence
+let a=100;
+//a++;
+++a;
+console.log(a);
