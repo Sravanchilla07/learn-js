df7ffcacdf0806c810d4c7a1e245253422854445
Sravanchilla07 <149558631+Sravanchilla07@users.noreply.github.com>
2023-11-06 05:13:58 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,14 @@
+let name="Sravan";
+let age=19;
+let male=true;
+// number =>2 to power 53
+//bigint
+//string =""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>value is not assigned
+//symbol=>unique and immutable
+//object
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null);
---
+++ test.js
@@ -0,0 +1,2 @@
+console.log("hello world")
+// JavaScript was invented by Bredan Eich in 1995.It was originally called Mocha, but was renamed LiveScript shortly after. 
